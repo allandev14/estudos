e4c31127ba4e5794071d752474bff96250f40216
allan sosusa <allanportinfo@gmail.com>
2020-11-17 17:27:45 -0300

    Adicionando mais dados

--- aulas-php/array-ass.php
+++ aulas-php/array-ass.php
@@ -1,4 +1,15 @@
 <?php
 //Array associativo
-$garota = array("nome"=> "Asuna", "idade"=> "23", "altura"=> "1.65");
-echo $pessoas["altura"];
+
+$garota = array("nome"=> "Asuna", "idade"=> "23", "altura"=> "1.65", "cidade"=> "Japão", "estado"=> "feliz");
+echo $garota["nome"];
+echo "<br />";
+echo $garota["idade"];
+echo "<br />";
+echo $garota["altura"];
+echo "<br />";
+echo $garota["cidade"];
+echo "<br />";
+echo $garota["estado"];
+echo "<br />";
+
