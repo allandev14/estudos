db8e6b3a1740abb88bd8ee9f187c096e8a082534
portadmin <allansousa@pop-os.localdomain>
2020-11-17 11:20:02 -0300

    Impressao de carros

---
+++ aulas-php/array.php
@@ -0,0 +1,23 @@
+<?php
+// Estudos de Array no PHP
+
+$carro1 = "Volvo 850 ";
+$carro2 = "Mercedes-Benz 450";
+$carro3 = "Aston Martin Rapide";
+$carro4 = "Rolls-Royce Corniche";
+
+echo $carro1;
+echo "<br />";
+echo "<hr />";
+
+echo $carro2;
+echo "<br />";
+echo "<hr />";
+
+echo $carro3;
+echo "<br />";
+echo "<hr />";
+
+echo $carro4;
+echo "<br />";
+echo "<hr />";
