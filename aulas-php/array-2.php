f20d5e65ec4d396e3901fa77f05d5b2436958796
allan sosusa <allanportinfo@gmail.com>
2020-11-17 15:24:09 -0300

    Metodo count

---
+++ aulas-php/array-2.php
@@ -0,0 +1,10 @@
+<?php
+//Continuação de Array
+//Com o comando do count podemos ver o valor dentro de uma variável
+
+$herois = ["homem-aranha", "homem-de-ferro", "Visão"];
+print_r($herois);
+echo "<br />";
+echo "<hr />";
+
+echo count($herois);
