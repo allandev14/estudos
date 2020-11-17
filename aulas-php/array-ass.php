4a900e3a0849c4f04083adeced6cdf500e13b34b
allan sosusa <allanportinfo@gmail.com>
2020-11-17 18:09:56 -0300

    foreach com exemplo

--- aulas-php/array-ass.php
+++ aulas-php/array-ass.php
@@ -13,6 +13,13 @@ echo "<br />";
 echo $garota["estado"];
 echo "<br />";
 echo "<hr />";
-foreach($garota as $indice => $valor){
-        echo $indice. ":" .$valor."<br>";
+foreach($garota as $indice => $valor) {
+        echo $indice. ":" .$valor."<br />";
+}
+echo "<br />";
+echo "<hr />";
+
+$time = array("Atacante"=> "João José", "idade"=> "18", "peso"=> "87.6", "posição"=> "esquerda");
+foreach($time as $jogo => $campeao) {
+        echo $jogo. ":" .$campeao."<br />";
 }
