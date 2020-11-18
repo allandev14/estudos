90600ca6bada19c2d62b5c794d518258b5ee17cd
allan sosusa <allanportinfo@gmail.com>
2020-11-18 09:30:41 -0300

    Funcoes dentro das arrays

---
+++ aulas-php/funcoes-de-arrays.php
@@ -0,0 +1,6 @@
+<?php
+//Funções dentro dos Arrays
+// A função is_array, indentifica se uma variável é realmente uma array
+
+$nomes  =array("Amanda", "Felipe", "Maria", "José");
+    echo is_array($nomes);
