0fa4a3298b825da4d6c0a7290b54fde4e78bbb4d
allan sosusa <allanportinfo@gmail.com>
2020-11-18 09:37:10 -0300

    is_array com looping

--- aulas-php/funcoes-de-arrays.php
+++ aulas-php/funcoes-de-arrays.php
@@ -2,5 +2,18 @@
 //Funções dentro dos Arrays
 // A função is_array, indentifica se uma variável é realmente uma array
 
+/*
 $nomes  =array("Amanda", "Felipe", "Maria", "José");
     echo is_array($nomes);
+*/
+
+$nomes = array("Amanda", "Felipe", "Maria", "José");
+var_dump(is_array($nomes)) ;
+echo "<br /> <br />";
+
+if(is_array($nomes)):
+    echo "É uma array";
+else:
+    echo "Não é uma array";
+endif;
+
