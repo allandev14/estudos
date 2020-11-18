5cec409243517304319fb67f48654e218ec74a93
allan sosusa <allanportinfo@gmail.com>
2020-11-18 09:03:34 -0300

    Arrays multidimensionais

--- aulas-php/array-ass.php
+++ aulas-php/array-ass.php
@@ -23,3 +23,47 @@ $time = array("Atacante"=> "João José", "idade"=> "18", "peso"=> "87.6", "posi
 foreach($time as $jogo => $campeao) {
         echo $jogo. ":" .$campeao."<br />";
 }
+echo "<br />";
+echo "<hr />";
+
+// Arrays multidimensionais
+// São arrays que contém um mais arrays internos
+
+$desenhos = array(
+        "anime" => array("Asuna", "garota", "anime", "japonesa", "doce"),
+        "manga" => array(5=>"Kirito", 6=>"garoto", 7=>"mangá", 8=>"japones", 9=>"espada-dupla"),
+        "naruto" => array(10=>"Sasuke", 11=>"uchira", 12=>"renegado", 13=>"japones", 14=>"espadachim"));
+
+echo $desenhos["anime"][0];
+    echo "<br/> <hr />";
+echo $desenhos["anime"][1];
+    echo "<br /> <hr />";
+echo $desenhos["anime"][2];
+    echo "<br /> <hr />";
+echo $desenhos["anime"][3];
+    echo "<br /> <hr />";
+echo $desenhos["anime"][4];
+    echo "<br /> <hr />";
+echo $desenhos["manga"][5];
+    echo "<br/ > <hr />";
+echo $desenhos["manga"][6];
+    echo "<br /> <hr />";
+echo $desenhos["manga"][7];
+    echo "<br /> <hr />";
+echo $desenhos["manga"][8];
+    echo "<br /> <hr />";
+echo $desenhos["manga"][9];
+    echo "<br /> <hr />";
+echo $desenhos["naruto"][10];
+    echo "<br /> <hr />";
+echo $desenhos["naruto"][11];
+    echo "<br /> <hr />";
+echo $desenhos["naruto"][12];
+    echo "<br /> <hr />";
+echo $desenhos["naruto"][13];
+    echo "<br /> <hr />";
+echo $desenhos["naruto"][14];
+    echo "<br /> <hr />";
+
+
+
