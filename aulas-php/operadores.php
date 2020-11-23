db35cf8c006362ee56bc1ca90370533a927a5b7e
allan sousa <allanportinfo@gmail.com>
2020-11-23 15:54:38 -0300

    adicionando mais arquivos

--- aulas-php/operadores.php
+++ aulas-php/operadores.php
@@ -4,20 +4,24 @@
 
 // Soma
 echo 1+1;
+echo "<br/>";
 echo 2+2+2+2+2+2;
 echo "<br/>";
 
 // Subtração
 echo 2-2;
+echo "<br/>";
 echo 3-1;
 echo "<br/>";
 
 // Divisão
 echo 10/2;
+echo "<br/>";
 echo 18/3;
 echo "<br/>";
 
 // Multiplicação
 echo 2*10;
+echo "<br>";
 echo 3*3;
 echo "<br/>";
