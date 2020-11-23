328b25c7c53e53862118cfba8c6d06f7592b482a
allan sousa <allanportinfo@gmail.com>
2020-11-23 12:36:47 -0300

    trabalhando com comando endif no php

--- aulas-php/condicionais.php
+++ aulas-php/condicionais.php
@@ -2,6 +2,8 @@
 
 //condicionais em php
 
+// Estudar a variável endif depois.
+
 $numero = 15;
 if ($numero == 15):
     echo "O número é 15";
@@ -11,8 +13,10 @@ endif;
     echo "<br/> <hr/>";
 
 $n1 = 26;
-if ($n1 == 26):
+if ($n1 == 3):
      echo "O númreo é igual a 26";
+elseif($numero <=9):
+    echo "È maior ou diferente de 9";
 else:
     echo "O número não é igual a 26";
 endif;
@@ -24,4 +28,4 @@ if ($nome == "Asuna"):
 else:
     echo "Nós vemos em breve";
 endif;
-
+echo "<br/> <hr/>";
