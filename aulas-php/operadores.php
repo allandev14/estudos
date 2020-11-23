ccd94591bac6deaaebb0d3873376a6f161dd6a46
allan sousa <allanportinfo@gmail.com>
2020-11-23 15:50:44 -0300

    mais operadores no codigo

---
+++ aulas-php/operadores.php
@@ -0,0 +1,23 @@
+<?php
+
+// Operadores aritimésticos
+
+// Soma
+echo 1+1;
+echo 2+2+2+2+2+2;
+echo "<br/>";
+
+// Subtração
+echo 2-2;
+echo 3-1;
+echo "<br/>";
+
+// Divisão
+echo 10/2;
+echo 18/3;
+echo "<br/>";
+
+// Multiplicação
+echo 2*10;
+echo 3*3;
+echo "<br/>";
