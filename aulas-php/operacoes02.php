7dae669ea3059b48ef5c32afc7dfa9f6c8e3d9f8
allan sousa <allanportinfo@gmail.com>
2020-11-23 17:49:23 -0300

    adionando exemplos

--- aulas-php/operacoes02.php
+++ aulas-php/operacoes02.php
@@ -2,3 +2,23 @@
 
 // Operadores de incrimento
 
+$valor = 100;
+
+// Pré-incremento echo ++$valor
+
+echo ++$valor;
+
+echo "<br/>";
+
+// Pós incremento echo $valor++
+
+echo $valor++;
+echo "<br/>";
+echo $valor;
+echo "<br/>";
+
+// Pré-decremento
+echo $valor--;
+echo "<br>";
+// Pós decremento
+echo $valor;
