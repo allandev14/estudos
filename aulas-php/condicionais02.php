bce8928cf58b98826f171ed742111b457e4c63e8
allan sousa <allanportinfo@gmail.com>
2020-11-23 13:47:37 -0300

    add condicionais02.php

---
+++ aulas-php/condicionais02.php
@@ -0,0 +1,17 @@
+<?php
+
+// Constastes em PHP parte 2
+
+$cor = "Azul";
+
+switch ($cor):    
+    case 'Azul':
+        echo "Então sua cor preferida é Azul";
+    break;
+    case 'Preto':
+        echo "Então sua cor preferida é Preto";
+    break;
+    case 'Laranja':
+        echo "Então sua cor preferida é o Laranja";
+    break;
+endswitch;
