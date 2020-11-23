c9120a0d42339a4cfe983f893b1ac4bccf83c3f9
allan sousa <allanportinfo@gmail.com>
2020-11-23 12:10:42 -0300

    operacoes com condicionais

--- aulas-php/condicionais.php
+++ aulas-php/condicionais.php
@@ -2,9 +2,17 @@
 
 //condicionais em php
 
-$numero == 15;
+$numero = 15;
 if ($numero == 15):
     echo "O número é 15";
 else:
     echo "O número não é igual a 15";
+endif;
+echo "<br>";
+
+$n1 = 26;
+if ($n1 == 26):
+     echo "O númreo é igual a 26";
+else:
+    echo "O número não é igual a 26";
 endif;
