396b0dbd6d21b9bd15411dba5c5baeb36495638b
allan sousa <allanportinfo@gmail.com>
2020-11-23 10:07:08 -0300

    func-arrays03.php

--- aulas-php/func-arrays03.php
+++ aulas-php/func-arrays03.php
@@ -2,6 +2,23 @@
 
 //array_sum — Calcula a soma dos elementos de um array
 
+//somá entre 2 números
+
 $a1 = array(30,20,);
 echo array_sum($a1);
+    echo "<br/>";
 
+$a2 = array(2,32,45,90);
+echo array_sum($a2);
+    echo "<br/>";
+
+$a3 = array(3,6,10,5);
+echo array_sum($a3);
+    echo "<br/>";
+
+$a4 = array(2,5,4,10);
+echo array_sum($a4);
+    echo "<br/>";
+
+$soma = array(25,2,5);
+echo array_sum($soma);
