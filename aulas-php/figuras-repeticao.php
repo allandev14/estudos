b44d7d415ca556f23c656ea4f98bdce06353de45
allan sousa <allanportinfo@gmail.com>
2020-11-24 12:18:57 -0300

    figuras de repeticao

--- aulas-php/figuras-repeticao.php
+++ aulas-php/figuras-repeticao.php
@@ -1,4 +1,19 @@
 <?php
 
 // Figuras de repetição While e Do While
+// While e do While
 
+$contador = 1;
+while ($contador <= 10):
+    echo "Contador é $contador <br/>";
+    $contador++;
+endwhile;
+echo "<br/> <hr/>";
+
+//Exemplo 2 
+
+$hora = 1;
+while ($hora <= 59):
+    echo "Agora são 12:$hora <br>";
+    $hora++;
+endwhile;
