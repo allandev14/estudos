ed1a4b55699df3c8f9bc52ca30eb6fc51d4707a6
allan sousa <allanportinfo@gmail.com>
2020-11-24 16:56:56 -0300

    estudos da funcao ceil

--- aulas-php/funcoes-numericas.php
+++ aulas-php/funcoes-numericas.php
@@ -16,10 +16,19 @@ $produto = number_format($a, 2, ",",".");
 echo "O valor do produto será de R$ $produto";
 echo "<br/> <hr/>";
 
-// round arredondamento
+// round realiza o arredondamento do para baixo ou para cima 
 echo round(3.6);
 echo "<br/>";
 echo round(7.4);
 echo "<br/>";
 echo round(5.1);
+echo "<br/> <hr/>";
+
+// ceil realiza o arredondamento somente para cima
+echo ceil(5.10);
 echo "<br/>";
+echo ceil(7.9);
+echo "<br/>";
+echo ceil(9.3);
+echo "<br/>";
+
