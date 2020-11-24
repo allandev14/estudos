a4ea3f1f427e90b2d0c9c44cedf55b709f8de565
allan sousa <allanportinfo@gmail.com>
2020-11-24 13:07:34 -0300

    estudo de foreach

--- aulas-php/for-foreach.php
+++ aulas-php/for-foreach.php
@@ -14,3 +14,12 @@ echo "<br/> <hr/>";
 for ($tabuada = 0; $tabuada <=10; $tabuada++):
     echo "3 x $tabuada = ". ($tabuada*3)."<br>";
 endfor;
+echo"<br/> <hr/>";
+
+// Foreach para cada
+
+$cores = array("Azul", "vermelho", "Amarelo", "Prata");
+foreach ($cores as $valor):
+    echo $valor ."<br/>";
+endforeach;
+echo "<br>";
