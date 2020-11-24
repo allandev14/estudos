29f04aaf6256518893051f54712bdc28ed19b4d6
allan sousa <allanportinfo@gmail.com>
2020-11-24 09:58:20 -0300

    inserindo operadores de maior, menor e identico

--- aulas-php/operadores-comparacao.php
+++ aulas-php/operadores-comparacao.php
@@ -50,3 +50,45 @@ if (10 !== 12): //Atributo de não idêntico
 else:
     echo "Negativo";
 endif;
+echo "<br/> <hr/>";
+
+// Operador diferente
+
+if (10 <> 10): //Operador de idêntico
+    echo "Positivo";
+else:
+    echo "Negativo";
+endif;
+echo "<br/> <hr>";
+
+// Operador de menor Quê
+if  (10 < 5): //Operador de menor quê
+    echo "Positivo";
+else:
+    echo "Negativo";
+endif;
+echo "<br/>";
+
+// Exemplo 2
+if (5 < 5):
+    echo "Positivo";
+else:
+    "Negativo";
+endif;
+echo "<br/> <hr/>";
+
+// Operador Maior Quê
+if (5 > 9):
+    echo "Positivo";
+else:
+    echo "Negativo";
+endif;
+echo "<br/> <hr/>";
+
+// Exemplo 2
+if (10 >2): // Operador de maior Quê
+    echo "Positivo";
+else:
+    echo "Negativo";
+endif;
+echo "<br/>";
