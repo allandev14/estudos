cfc4a344f80fa533e338297218954bbe59301a15
allan sousa <allanportinfo@gmail.com>
2020-11-24 11:07:28 -0300

    estudo do operador && ou and

--- aulas-php/operadores-log.php
+++ aulas-php/operadores-log.php
@@ -9,8 +9,20 @@ $nome = "Amanda";
 /* e Verdadeiro (TRUE) se tanto $a quanto $b são verdadeiros.
 Pode ser representado por && ou um and */
 
-if (($idade == 25) && ($nome == "Amanda")):
+if (($idade == 25) && ($nome == "Amanda")): //Retorna um valor verdadeiro quando as duas expressões são verdadeiras
     echo "É verdadeiro";
 else:
     echo "Não é verdadeiro";
+endif;
+echo "<br/> <hr/>";
+
+// Exemplo 2
+
+$animal = "cachorro";
+$porte = "pequeno";
+
+if(($animal == "cachorro") and ($porte == "pequeno")):
+    echo "Você tem um $animal";
+else:
+    echo "VocÊ não tem um animal de estimação";
 endif;
