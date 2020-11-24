67d52779aec5f73f9c2f46bcca566bb865991325
allan sousa <allanportinfo@gmail.com>
2020-11-24 16:50:03 -0300

    estudando sobre round

--- aulas-php/funcoes-numericas.php
+++ aulas-php/funcoes-numericas.php
@@ -2,7 +2,7 @@
 
 // Funções Númericas
 
-// Exemplo 1
+// number_format formata a unidade númerica e realiza a apresentação da mesma na tela
 
 $db = 1234.56;
 $preco = number_format($db, 2, ",",".");
@@ -14,3 +14,12 @@ echo "<br/> <hr/>";
 $a = 2351.23;
 $produto = number_format($a, 2, ",",".");
 echo "O valor do produto será de R$ $produto";
+echo "<br/> <hr/>";
+
+// round arredondamento
+echo round(3.6);
+echo "<br/>";
+echo round(7.4);
+echo "<br/>";
+echo round(5.1);
+echo "<br/>";
