ad0ded55a1d7f285fff51c82b313e2f83417ac1c
allan sousa <allanportinfo@gmail.com>
2020-11-24 11:57:08 -0300

    alteracoes em operadores-logicos

--- aulas-php/operadores-log.php
+++ aulas-php/operadores-log.php
@@ -48,8 +48,44 @@ echo "<br/>";
 $b2 = 12;
 $b3 = 25;
 
-if (($b2 == 12) or ($b3 == 25)):
+if (($b2 == 12) || ($b3 == 25)):
     echo "É verdadeiro";
 else:
     echo "Não é verdadeiro";
 endif;
+echo "<br/> <hr/>";
+
+/* Operador Ou exclusivo xor retorna uma valor positivo quando apenas uma das expressões seja verdadeira
+Se os dois forem verdadeiro ele irá retorna o valor como falso */
+
+$dia = 24;
+$mes = "novembro";
+if (($dia == 24) xor ($mes == "novembro")):
+    echo "Sim hoje é dia $dia";
+else:
+    echo "Não hoje não é dia $dia";
+endif;
+echo "<br/> <hr/>";
+
+// Exemplo 2 xor 
+
+$terra = "planeta";
+$lua = "satelite-natural";
+if (($terra == "barco") xor ($lua == "satelite-natural")):
+    echo "Verdadeiro";
+else:
+    echo "Falso";
+endif;
+echo "<br/> <hr/>";
+
+// Operador de negação ! como o nome sugere realiza o negação dá expressão indicada na sentença a frente
+
+$computador = "tela";
+$mesa = "cadeira";
+
+if (!("computador" == "tela") and ($mesa == "cadeira")):
+    echo "O valor é verdadeiro";
+else:
+    echo "O valor é falso";
+endif;
+
