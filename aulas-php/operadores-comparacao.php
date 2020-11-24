81eb59948499607dfd976b0feb9627154a7683df
allan sousa <allanportinfo@gmail.com>
2020-11-24 09:19:25 -0300

    operadores-comparacao.php

--- aulas-php/operadores-comparacao.php
+++ aulas-php/operadores-comparacao.php
@@ -2,3 +2,15 @@
 
 // Operadores de comparação
 
+if (10 == 10):
+    echo "Positivo";
+else:  
+    echo "Negativo";
+endif;
+echo "<br/>";
+
+if (20 == 32):
+    echo "Positivo";
+else:
+    echo "Negativo";
+endif;
