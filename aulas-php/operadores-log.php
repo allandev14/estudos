8e6ca62e2f9e8785634cdf7ae8ff472685474965
allan sousa <allanportinfo@gmail.com>
2020-11-24 11:34:08 -0300

    operador Or

--- aulas-php/operadores-log.php
+++ aulas-php/operadores-log.php
@@ -3,6 +3,8 @@
 // Operadores Lógicos
 // Nós permite realizar comparações entre expressões
 
+// Operador E && ou and
+
 $idade = 25;
 $nome = "Amanda";
 
@@ -24,5 +26,30 @@ $porte = "pequeno";
 if(($animal == "cachorro") and ($porte == "pequeno")):
     echo "Você tem um $animal";
 else:
-    echo "VocÊ não tem um animal de estimação";
+    echo "Você não tem um animal de estimação";
+endif;
+echo "<br/> <hr/>";
+
+// Operador Ou || ou Or 
+// Retorna verdeiro se ao menos uma expressão for declarada verdadeira
+
+$a = 24;
+$b = "Alessandra";
+
+if (($a == 22) or ($b == "Alessandra")):
+    echo "É verdadeiro";
+else:
+    echo "Não é verdadeiro";
+endif;
+echo "<br/>";
+
+// Exemplo 2
+
+$b2 = 12;
+$b3 = 25;
+
+if (($b2 == 12) or ($b3 == 25)):
+    echo "É verdadeiro";
+else:
+    echo "Não é verdadeiro";
 endif;
