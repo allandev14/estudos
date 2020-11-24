27d1a1ebe7d977244ccd7ada90e4789e9cb7fed6
allan sousa <allanportinfo@gmail.com>
2020-11-24 09:44:38 -0300

    operador nao identico

--- aulas-php/operadores-comparacao.php
+++ aulas-php/operadores-comparacao.php
@@ -27,8 +27,26 @@ echo "<br/> <hr/>";
 
 // Operador idêntico
 
-if (1 === 1): //Atribuição de idêntidade
+if (1 === 101): //Atribuição de idêntidade
     echo "Positivo";
+else:
+    echo "Negativo";
+endif;
+echo "<br/>";
+
+// Exemplo 2
+
+if (2 === 52):
+    "Verdadeiro";
+else:
+    "Falso";
+endif;
+echo "<br/> <hr/>";
+
+// Não indêntico
+
+if (10 !== 12): //Atributo de não idêntico
+    echo "Postivo";
 else:
     echo "Negativo";
 endif;
