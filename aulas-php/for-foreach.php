e01ff325c30cd9815af13345a829bf8f3580156c
allan sousa <allanportinfo@gmail.com>
2020-11-24 12:53:21 -0300

    tabuada com for

--- aulas-php/for-foreach.php
+++ aulas-php/for-foreach.php
@@ -7,5 +7,10 @@
 for ($timer = 0; $timer <= 59; $timer++):
     echo "Agora são 12hs e $timer.s<br>";
 endfor;
+echo "<br/> <hr/>";
 
+// Exemplo de tabuada
 
+for ($tabuada = 0; $tabuada <=10; $tabuada++):
+    echo "3 x $tabuada = ". ($tabuada*3)."<br>";
+endfor;
