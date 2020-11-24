c04fc13022504d6214af4cbb5a51d30465cce392
allan sousa <allanportinfo@gmail.com>
2020-11-24 16:41:07 -0300

    exemplo 2 funcoes-numericas.php

--- aulas-php/funcoes-numericas.php
+++ aulas-php/funcoes-numericas.php
@@ -2,6 +2,15 @@
 
 // Funções Númericas
 
+// Exemplo 1
+
 $db = 1234.56;
 $preco = number_format($db, 2, ",",".");
 echo "O valor do produto é R$ $preco";
+echo "<br/> <hr/>";
+
+// Exemplo 2
+
+$a = 2351.23;
+$produto = number_format($a, 2, ",",".");
+echo "O valor do produto será de R$ $produto";
