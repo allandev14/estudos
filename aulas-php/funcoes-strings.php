54872a0072066dc475f669927fe36a085087040a
allan sousa <allanportinfo@gmail.com>
2020-11-24 13:33:05 -0300

    exemplo 1

--- aulas-php/funcoes-strings.php
+++ aulas-php/funcoes-strings.php
@@ -1,3 +1,13 @@
 <?php
 
 // Funções para strings
+// strtoupper Converte strings minuscúlas para maiuscúlas
+
+$nome = "adriana cubas";
+$novoNome = strtoupper($nome);
+echo $novoNome;
+echo "<br/> <hr/>";
+
+$uchira = "itachi uchira";
+$susano = strtoupper($uchira);
+echo $susano;
