1a8eb86db37f8764a9df38d737b73c8ddf5229d9
allan sousa <allanportinfo@gmail.com>
2020-11-25 14:20:35 -0300

    adicionando condicionais

--- formulario/index.php
+++ formulario/index.php
@@ -1,3 +1,16 @@
+<?php
+if (isset($_POST['enviar-formulario'])):
+    echo "Enviou";
+else:
+    echo "Não enviou";
+endif;
+echo "<br>";
+
+var_dump($_POST);
+
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +20,14 @@
 </head>
 <body>
     <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST"> <!-- Nesse caso a variável GLOBAL faz referencia ao nome do proprio documento!-->
-        Idade: <input type="text" name="idade"><br> <br>
+        Idade: <input type="text" name="idade"><br>
         Email: <input type="email" name="email"><br>
         Peso: <input Type="text" name="Peso"><br>
         IP: <input type="text" name="ip"><br>
         URL: <input type="text" name="url">
-        <button type='submit'>Enviar</button> <br>
+        <button type='submit' name="enviar-formulario">Enviar</button> <br>
     </form>
-    <a href='dados.php?idade=25&sobrenome=Souza'>Enviar dados</a><!--Podemos adicionar um parametro no arquivo php
+    <!--<a href='dados.php?idade=25&sobrenome=Souza'>Enviar dados</a>Podemos adicionar um parametro no arquivo php
     inserindo ? para indicar o inicio de um metódo e & para indicar que teremos mais um metódo!-->
 </body>
 </html>
