5beae4fe846ed06784b3b8ed2525e99c287778c4
allan sousa <allanportinfo@gmail.com>
2020-11-25 10:31:19 -0300

    adicionando variaveis $_SERVE

--- aulas-php/variaveis-globais.php
+++ aulas-php/variaveis-globais.php
@@ -27,5 +27,9 @@ echo "<br/> <hr/>";
 
 // Super $_SERVER é uma arrar que guarda vários indices
 
-echo $_SERVER['PHP_SELF']. "<br/>";
-echo $_SERVER['SERVER_NAME']."<br/>";
+echo $_SERVER['PHP_SELF']. "<br/>";  //variável identifica qual o caminho do arquivo PHP na máquina local
+echo $_SERVER['SERVER_NAME']."<br/>"; // variável mostra qual o servidor está alocando o PHP
+echo $_SERVER['SCRIPT_FILENAME']. "<br/>"; // variável mostra qual o caminho absoluto do SCRIPT
+echo $_SERVER['DOCUMENT_ROOT']. "<br/>"; // Variável mostra o caminho absoluto do documento
+echo $_SERVER['SERVER_PORT']. "<br/>"; // variável mostra o camnho da porta do servidor 
+ 
