eb16a3c66e80d033ed0896318998c7fc4b1d98db
allan sousa <allanportinfo@gmail.com>
2020-11-25 11:05:15 -0300

    criando um novo formulario

---
+++ formulario/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        Nome: <input type="text" name="nome">
+        Email: <input type="email" name="Email">
+        <input type="submit" name="enviar">
+    </form>
+</body>
+</html>
