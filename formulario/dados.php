09e056b7ab40262306482921578f8fc9f2a8220d
allan sousa <allanportinfo@gmail.com>
2020-11-25 11:53:36 -0300

    adicioando arquivos

--- formulario/dados.php
+++ formulario/dados.php
@@ -1,9 +1,13 @@
 <?php
-
+/*
 $nome = $_POST['nome'];
 $email = $_POST['email'];
 
 echo "Seu nome é $nome e seu email é $email";
 echo "<br>";
-var_dump($_POST);
+*/
+
+echo $_GET['idade']."<br>".$_GET['sobrenome']."<br>";
+
+var_dump($_GET);
 
--- formulario/index.php
+++ formulario/index.php
@@ -9,7 +9,9 @@
     <form action="dados.php" method="POST">
         Nome: <input type="text" name="nome"><br> <br>
         Email: <input type="email" name="email"><br>
-        <button type='submit'>Enviar</button>
+        <button type='submit'>Enviar</button> <br>
     </form>
+    <a href='dados.php?idade=25&sobrenome=Souza'>Enviar dados</a><!--Podemos adicionar um parametro no arquivo php
+    inserindo ? para indicar o inicio de um metódo e & para indicar que teremos mais um metódo!-->
 </body>
 </html>
