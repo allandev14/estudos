8b04d77703e31f5f39cffd7e358c6a845550fa08
allan sousa <allanportinfo@gmail.com>
2020-11-25 14:02:56 -0300

    fazendo mudanças no projeto

--- formulario/dados.php
+++ formulario/dados.php
@@ -5,7 +5,7 @@ $email = $_POST['email'];
 
 echo "Seu nome é $nome e seu email é $email";
 echo "<br>";
-*/
+
 
 echo $_GET['idade']."<br>".$_GET['sobrenome']."<br>";
 
---
+++ super-globais02.php
@@ -0,0 +1,11 @@
+<?php
+
+// Estudos de super globais 02
+
+/*
+variável  $_POST Um array associativo de variáveis passados para o script atual 
+via método HTTP POST quando utilizado application/x-www-form-urlencoded 
+ou multipart/form-data como valor do cabeçalho HTTP Content-Type na requisição.
+*/
+
+// Variável $_GET Um array associativo de variáveis passadas para o script atual via o método HTTP GET.
