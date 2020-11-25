e042f8cc6e009406397dd1de36ceaf0a2339e433
allan sousa <allanportinfo@gmail.com>
2020-11-25 10:06:55 -0300

    exemplo 02 com variavies-globais

--- aulas-php/variaveis-globais.php
+++ aulas-php/variaveis-globais.php
@@ -12,3 +12,15 @@ function soma() {
 }
 
 soma();
+echo "<br/> <hr/>";
+
+// Exemplo 2
+$a = 120;
+$b = 10;
+
+function soma02() {
+    echo $GLOBALS['a'] + $GLOBALS['b'];
+}
+
+soma02();
+echo "<br/> <hr/>";
